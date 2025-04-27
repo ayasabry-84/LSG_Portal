7e6b517c5796b4cee7077ea32262e9dbe7a34efa
ayasabry-84 <ayasabryy017@gmail.com>
2025-04-27 19:12:09 +0300

    delete to organize files

--- views.py
+++
@@ -1,8 +0,0 @@
-# myapp/views.py
-from rest_framework import viewsets
-from .models import Meal
-from .serializers import MealSerializer
-
-class MealViewSet(viewsets.ModelViewSet):
-    queryset = Meal.objects.all()
-    serializer_class = MealSerializer
